19ab323d6f1781af08309f96b600e0ae59fe02fb
Ricardo <rmvnew@gmail.com>
2023-02-15 08:39:54 -0400

    feat: hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.devsuperior.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(long workedId,int days) {
+		return new Payment("Bob",200.00,days);
+	}
+
+}
